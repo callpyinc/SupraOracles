cdbba6725573489fab8e931c63b8907197928c79
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-11-16 02:10:55 -0500

    Update readPricefeedFlipMe.js

--- scripts/readPricefeedFlipMe.js
+++ scripts/readPricefeedFlipMe.js
@@ -3,7 +3,7 @@ const Web3 = require('web3')
 const rpcURL = "https://liberty20.shardeum.org/"
 const web3 = new Web3(rpcURL)
 
-const simpleStorageAddress = '0x704a20CbCB84cf61F1a8045E3b964837C12565Bd'
+const simpleStorageAddress = '0xf7D35599fA6a7e669503a4b6842924565160fB36'
 const simpleStorageABI = [{"inputs":[],"name":"btcUsdt","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"ethUsdt","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"flipMe","outputs":[{"internalType":"int256","name":"","type":"int256"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"sValueFeed","outputs":[{"internalType":"contractISupraSValueFeed","name":"","type":"address"}],"stateMutability":"view","type":"function"}]
 
 const simpleStorageDeployed = new web3.eth.Contract(simpleStorageABI, simpleStorageAddress)
